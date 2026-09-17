32a01ef27d4ffba0ec61104439d54054e75bd810
Astha Sahu <134774307+aastha51551@users.noreply.github.com>
2026-09-17 10:16:57 +0530

    Added reloader

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,21 @@ export default function App() {
     const interval = setInterval(fetchCount, 6000);
     return () => clearInterval(interval);
   }, []);
-
+  const url = `https://www.wridrot.com/`; 
+  const interval = 30000; 
+  
+  //Reloader Function
+  function reloadWebsite() {
+    axios.get(url)
+      .then(response => {
+        console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+      })
+      .catch(error => {
+        console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+      });
+  }
+  
+  setInterval(reloadWebsite, interval);
   
   useEffect(() => {
     const checkUrlState = async () => {
